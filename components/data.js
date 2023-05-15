e1d2cd58c6697503988f14a24cd4d6ac67dae26f
HavertzZ04 <havertzz04@gmail.com>
2023-05-15 00:02:04 -0500

    feat: :bug: Problem with the media rute

--- components/data.js
+++ components/data.js
@@ -2,25 +2,25 @@ let data = [
     {
         "title": "Supersonic",
         "artist": "Skrillex",
-        "logo":    "../images/skrillex.jpg",
+        "logo": "../../images/skrillex.jpg",
         "duration": "165",
-        "song": `../music/supersonic.mp3`,
+        "song": "../../music/supersonic.mp3",
         "id": "1"
     },
     {
         "title": "Royalty",
         "artist": "Egzod & Maestro Chives",
-        "logo": "../images/royalty.jpg",
+        "logo": "../../images/royalty.jpg",
         "duration": "224",
-        "song": `../music/royalty.mp3`,
+        "song": "../../music/royalty.mp3",
         "id": "2"
     },
     {
         "title": "We Are",
         "artist": "Jo Cohen & Sex Whales",
-        "logo": "../images/weare.jpg",
+        "logo": "../../images/weare.jpg",
         "duration": "236",
-        "song": `../music/weare.mp3`,
+        "song": "../../music/weare.mp3",
         "id": "3"
     }
 ]
