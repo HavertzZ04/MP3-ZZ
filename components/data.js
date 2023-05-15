a5eae7eb8ce1c31f7b8353800565c4119b125d45
havertzz04 <havertzz04@gmail.com>
2023-05-15 09:08:25 -0500

    feat: :lipstick: Multimedia Files

--- components/data.js
+++ components/data.js
@@ -2,26 +2,50 @@ let data = [
     {
         "title": "Supersonic",
         "artist": "Skrillex",
-        "logo": "../images/skrillex.jpg",
+        "logo": "./images/skrillex.jpg",
         "duration": "165",
-        "song": "../music/supersonic.mp3",
+        "song": "./music/supersonic.mp3",
         "id": "1"
     },
     {
         "title": "Royalty",
         "artist": "Egzod & Maestro Chives",
-        "logo": "../images/royalty.jpg",
+        "logo": "./images/royalty.jpg",
         "duration": "224",
-        "song": "../music/royalty.mp3",
+        "song": "./music/royalty.mp3",
         "id": "2"
     },
     {
         "title": "We Are",
         "artist": "Jo Cohen & Sex Whales",
-        "logo": "../images/weare.jpg",
+        "logo": "./images/weare.jpg",
         "duration": "236",
-        "song": "../music/weare.mp3",
+        "song": "./music/weare.mp3",
         "id": "3"
+    },
+    {
+        "title": "IDFC (Tarro RMX)",
+        "artist": "Blackbear",
+        "logo": "./images/idfc.jpg",
+        "duration": "249",
+        "song": "./music/idfc.mp3",
+        "id": "4"
+    },
+    {
+        "title": "Silence",
+        "artist": "Marshmello & Khalid",
+        "logo": "./images/silence.jpg",
+        "duration": "180",
+        "song": "./music/silence.mp3",
+        "id": "5"
+    },
+    {
+        "title": "Ballin",
+        "artist": "Logic",
+        "logo": "./images/ballin.jpg",
+        "duration": "167",
+        "song": "./music/ballin.mp3",
+        "id": "6"
     }
     
 ]
