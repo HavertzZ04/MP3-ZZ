f5688b920cbffbb375793b8eb8a91ead4abb0170
HavertzZ04 <havertzz04@gmail.com>
2023-05-14 23:41:11 -0500

    feat: :zap: Data Base
    I made a little kind of data base in order to add songs

---
+++ components/data.js
@@ -0,0 +1,28 @@
+let data = [
+    {
+        "title": "Supersonic",
+        "artist": "Skrillex",
+        "logo":    "../images/skrillex.jpg",
+        "duration": "165",
+        "song": `../music/supersonic.mp3`,
+        "id": "1"
+    },
+    {
+        "title": "Royalty",
+        "artist": "Egzod & Maestro Chives",
+        "logo": "../images/royalty.jpg",
+        "duration": "224",
+        "song": `../music/royalty.mp3`,
+        "id": "2"
+    },
+    {
+        "title": "We Are",
+        "artist": "Jo Cohen & Sex Whales",
+        "logo": "../images/weare.jpg",
+        "duration": "236",
+        "song": `../music/weare.mp3`,
+        "id": "3"
+    }
+]
+
+export default data;
