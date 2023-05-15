de786cbbdc03dee7e42905f100e80e1f9db1c0bc
havertzz04 <havertzz04@gmail.com>
2023-05-15 09:09:20 -0500

    feat: :zap: Improve HTML

--- html/myMain.js
+++ html/myMain.js
@@ -11,30 +11,31 @@ export default {
     song: data[0].song,
 
     show(){
-        document.querySelector("body").insertAdjacentHTML("beforeend",         `
-        <div class="container" id="main">
-        <div class="row">
-            <h1 class="text-center" id="title">${this.title}</h1>
-            <h4 class="text-center" id="artist">${this.artist}</h4>
-            <img src=${this.logo} alt="logo" id="logo"> 
-            <div id="bar">  
-                <div class="progress col-12 ps-0 pe-0">
-                    <div class="progress-bar barCustom progress-bar-striped ms-0" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" id="progressBar"></div>
+        document.querySelector("#main").insertAdjacentHTML("beforeend",         `
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center" id="title">${this.title}</h1>
+                    <h4 class="text-center" id="artist">${this.artist}</h4>
+                    <img src=${this.logo} alt="logo" id="logo"> 
+                    <div id="bar">  
+                        <div class="progress col-12 ps-0 pe-0">
+                            <div class="progress-bar barCustom progress-bar-striped ms-0" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" id="progressBar"></div>
+                        </div>
+                    </div>  
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-6 text-start time" id="time">${this.start}</div>
+                            <div class="col-6 text-end time" id="songTime"></div>
+                        </div>
+                    <div> 
+                    <div id="buttonsGroup" class="text-center">
+                        <button class="buttons" id="previous">${this.previous}</button>
+                        <button class="buttons" id="play">${this.play}</button>
+                        <button class="buttons" id="next">${this.next}</button>
+                        <audio id="song" src="${this.song}" controls></audio>
+                    </div>
                 </div>
-            </div>  
-            <div class="container">
-                <div class="row">
-                    <div class="col-6 text-start time" id="time">${this.start}</div>
-                    <div class="col-6 text-end time" id="songTime"></div>
-                </div>
-            <div> 
-            <div id="buttonsGroup" class="text-center">
-                <button class="buttons" id="previous">${this.previous}</button>
-                <button class="buttons" id="play">${this.play}</button>
-                <button class="buttons" id="next">${this.next}</button>
-                <audio id="song" src="${this.song}" controls></audio>
-            </div>
-        </div>`
+            </div>`
         );
     }
 }
