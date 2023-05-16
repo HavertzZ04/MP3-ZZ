c0f3a43adce68d4c11ae6623dc1c01e9e73e2e01
havertzz04 <havertzz04@gmail.com>
2023-05-16 09:20:34 -0500

    feat: :art: Module Code

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import myMain from "./html/myMain.js";
+import myTable from "./html/myTable.js";
 import table from "./components/table.js";
 import logic from "./components/logic.js";
 
-
+myTable.show();
 myMain.show();
 table.show();
 logic();
